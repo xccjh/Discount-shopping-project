9e422044420fb10ee8e219d728fad113599fb7ba
itcastwsy <yeah126139163@163.com>
2019-03-09 09:52:10 +0800

    首页轮播图动态渲染

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,37 @@
-$(function () {
-  var gallery = mui('.mui-slider');
-gallery.slider({
-  interval:5000//自动轮播周期，若为0则不自动播放，默认为0；
+$(function() {
+  init();
+
+  // js一加载就执行的 方法 入口
+  function init() {
+    swiperdata();
+  }
+
+  // 获取首页轮播图的数据
+  function swiperdata() {
+    $.ajax({
+      url: "http://api.pyg.ak48.xyz/api/public/v1/home/swiperdata",
+      type: "get",
+      success: function(result) {
+        //  判断请求是否成功
+        if (result.meta.status === 200) {
+          // 获取要渲染的数据 data=[]
+          let data = result.data;
+          // console.log(data);
+
+          // 生成要渲染的html标签
+          let html = template("swiperTpl", { arr: data });
+          // 把标签插入到 轮播图的 div中
+          $(".pyg_slide").html(html);
+
+          // 初始化轮播图
+          var gallery = mui(".mui-slider");
+          gallery.slider({
+            interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+          });
+        } else {
+          console.log("请求失败", result);
+        }
+      }
+    });
+  }
 });
-  
-})
