620145046a8e11b68ec7d31c4dd9ef0b37dc936e
itcastwsy <yeah126139163@163.com>
2019-03-12 11:44:01 +0800

    商品详情动态渲染

--- src/js/good_detail.js
+++ src/js/good_detail.js
@@ -11,7 +11,27 @@ $(function () {
     $.get("http://api.pyg.ak48.xyz/api/public/v1/goods/detail",{
       goods_id:getUrl("goods_id")
     },function (result) {
-      console.log(result);
+      // console.log(result);
+      if(result.meta.status==200){
+
+        //  模板需要渲染 整个商品的详情部分  所有的数据都要传递到 模板引擎中
+        let data=result.data;
+        // data={... pics:[]}
+        let html=template("mainTpl",data);
+        // let html=template("mainTpl",{pics:data.pics,goods_name:艾美特});
+
+        // let html=template("id",{arr:result.data})   第二个参数的格式是一个对象
+
+        $(".pyg_view").html(html);
+
+        // 轮播图的js 初始
+        var gallery = mui(".mui-slider");
+        gallery.slider({
+          interval: 5000 //自动轮播周期，若为0则不自动播放，默认为0；
+        });
+      }else{
+        console.log("请求失败",reslut);
+      }
       
     })
     
