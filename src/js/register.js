828bd4c304998dff765cbaee18800a97c0406ee7
itcastwsy <yeah126139163@163.com>
2019-03-13 10:39:43 +0800

    注册页面注册功能

--- src/js/register.js
+++ src/js/register.js
@@ -10,6 +10,15 @@ $(function() {
     3 时间到了 
       1 清除定时器
       2 重新启用 按钮
+  3 绑定注册点击事件
+    1 获取一坨值 表单一些值
+    2 挨个验证
+      1 不通过弹出对话框 提示
+    3 构造参数 发送请求到后台 完成注册
+      1 注册失败  手机号码已经被注册过了 弹出一个提示 
+    4 注册成功
+      1 弹出一个提示 恭喜用户 你进坑。。
+      2 跳转页面 登录页面
 
    */
   init();
@@ -44,31 +53,110 @@ $(function() {
           mobile: mobile_txt
         },
         function(result) {
-          if(result.meta.status==200){
+          if (result.meta.status == 200) {
             console.log(result);
-            // 成功 
+            // 成功
             // 2.1 禁用按钮
-             $(".get_code_btn").attr("disabled","disabled");
+            $(".get_code_btn").attr("disabled", "disabled");
             //  2.2 开启 定时器
             // 要倒计时的总时间 5s
-            let time=5;
+            let time = 5;
             $(".get_code_btn").text(`${time} 秒后再获取`);
-            let timeId=setInterval(() => {
+            let timeId = setInterval(() => {
               time--;
               $(".get_code_btn").text(`${time} 秒后再获取`);
               // 2.3 判断时间是否到期了
-              if(time==0){
+              if (time == 0) {
                 // 2.3.1 清除定时器
                 clearInterval(timeId);
                 // 2.3.2 启用按钮 修改按钮文字
-                $(".get_code_btn").removeAttr("disabled").text("获取验证码");
+                $(".get_code_btn")
+                  .removeAttr("disabled")
+                  .text("获取验证码");
               }
             }, 1000);
-          }else{
-            console.log("失败",result);
+          } else {
+            console.log("失败", result);
           }
         }
       );
+    });
+
+    // 绑定 点击注册 的按钮 事件
+    $(".register_btn ").on("tap", function() {
+      // 3.1 获取一堆值
+      let mobile_text = $("input[name='mobile']")
+        .val()
+        .trim();
+      let code_text = $("input[name='code']")
+        .val()
+        .trim();
+      let email_text = $("input[name='email']")
+        .val()
+        .trim();
+      let pwd_text = $("input[name='pwd']")
+        .val()
+        .trim();
+      let pwd2_text = $("input[name='pwd2']")
+        .val()
+        .trim();
+      let gender_text = $("input[name='gender']:checked")
+        .val()
+        .trim();
+
+      // 3.2 按个验证
+      // 验证手机号码
+      if (!checkPhone(mobile_text)) {
+        //  失败
+        // 1.2 弹出一个提示
+        mui.toast("手机号码错误");
+
+        return;
+      }
+
+      // 验证 验证码的长度 不等于  4 就是错误
+      if (code_text.length != 4) {
+        mui.toast("验证码不合法");
+        return;
+      }
+
+      // 验证邮箱的合法性
+      if (!checkEmail(email_text)) {
+        mui.toast("邮箱不合法");
+        return;
+      }
+
+      // 验证密码的合法性
+      if (pwd_text.length < 6) {
+        mui.toast("密码格式不对");
+        return;
+      }
+      // 验证 重复密码
+      if (pwd2_text != pwd_text) {
+        mui.toast("两次密码不一致！");
+        return;
+      }
+
+      // 3.3 构造参数 完成注册
+      $.post("http://api.pyg.ak48.xyz/api/public/v1/users/reg", {
+        mobile: mobile_text,
+        code: code_text,
+        email: email_text,
+        pwd: pwd_text,
+        gender: gender_text
+      },function (result) {
+        console.log(result);
+        if(result.meta.status==200){
+          // 成功
+          mui.toast("注册成功");
+          setTimeout(() => {
+            location.href="login.html";
+          }, 1000);
+        }else{
+         mui.toast(result.meta.msg)
+        }
+        
+      });
     });
   }
 
@@ -78,6 +166,16 @@ $(function() {
       return false;
     } else {
       return true;
+    }
+  }
+
+  // 验证 邮箱
+  function checkEmail(myemail) {
+    var myReg = /^[a-zA-Z0-9_-]+@([a-zA-Z0-9]+\.)+(com|cn|net|org)$/;
+    if (myReg.test(myemail)) {
+      return true;
+    } else {
+      return false;
     }
   }
 });
