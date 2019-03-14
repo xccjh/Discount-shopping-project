47b38c1e5ef50932128bec79fddc8b46aad79a60
itcastwsy <yeah126139163@163.com>
2019-03-14 11:04:21 +0800

    购物车动态渲染

--- src/js/cart.js
+++ src/js/cart.js
@@ -6,28 +6,61 @@ $(function () {
       假如 没有数据 
         1 把当前的页面路径存入到 本地存储中 pageurl
         2 跳转到登录页面
-  
+  2 发送请求渲染数据 私人的数据  token  请求头 
+    1 token  本地存储 复杂类型
    */
   init();
   function init() {
-    // checkPermission();
+    cartAll();
+  }
+
+  // 获取购物车数据
+  function cartAll() {
+
+    // let obj={ name:"猪八戒", skill:"卖队友" };
+    // // es5 
+    // // let name=obj.name;
+    // // let skill=obj.skill;
+    // // es6  解构的语法
+    // let {name,skill}=obj;
+    // console.log(name,skill);
+
+
+    // 2.1 获取本地存储中的token 字段
+    let token=JSON.parse(sessionStorage.getItem("userinfo")).token;
+    // es6  解构 node js es 6  解构 知识点
+    // let {token}=JSON.parse(sessionStorage.getItem("userinfo"));
+
+    // $.get 没有办法 设置请求头
+    $.ajax({
+      url:"http://api.pyg.ak48.xyz/api/public/v1/my/cart/all",
+      // type:"get"
+      // data:{}
+      headers:{
+        "Authorization" : token
+      },
+      success:function (result) {
+        // console.log(result);
+        if(result.meta.status==200){
+          // 以前循环的数据都是数组 但是现在 是对象
+          // 数组可以循环 对象可以循环
+          // 要传递给模板引擎的数据  
+          let cart_info=JSON.parse(result.data.cart_info);
+          console.log(cart_info);
+          let html=template("mainTpl",{goodsObj:cart_info});
+          $(".order_list").html(html);
+
+          // 数字输入框的初始化
+          mui(".mui-numbox").numbox()
+
+        }else{  
+          console.log("失败",result);
+        }
+        
+      }
+    })
+    
   }
   
-  // // 验证权限  权限
-  // function checkPermission() {
-  //   /* 
-  //   1 在浏览器中 css html先加载 最后 才到 js加载 所以肯定闪一闪 
-  //   2 把js 放到 head标签中
-  //    */
-  //   // 1 获取本地存储中的 userinfo 数据
-  //   let userinfoStr=sessionStorage.getItem("userinfo");
-  //   // 2 判断是否存在
-  //   if(!userinfoStr){
-  //     // 2.1 存一下 当前的页面
-  //     sessionStorage.setItem("pageurl",location.href);
-  //     // 2.2 跳转页面 登录页面  不要 加延迟 直接跳转！！！！！
-  //     location.href="login.html";
-  //     return;
-  //   }
-  // }
+
 })
